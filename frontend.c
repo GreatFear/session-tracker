302e221c095633f99233880deae1fea12c6dfef5
GreatFear <no>
2026-06-11 23:04:00 -0500

    Updated something

--- frontend.c
+++ frontend.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 typedef struct {
-  GtkWidget *entry;
+  GtkWidget *entry_topic;
+  GtkWidget *entry_goal;
   GtkWidget *result_label;
 } AppWidgets;
 
@@ -23,16 +24,19 @@ static void start_application(GtkApplication *app, gpointer user_data)
   GtkWidget *topic_prompt = gtk_label_new("What is the topic of this session?");
   gtk_box_pack_start(GTK_BOX(vbox), topic_prompt, FALSE, FALSE, 0);
 
+  widgets->entry_topic = gtk_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(widgets->entry_topic), "Enter Topic");
+  gtk_box_pack_start(GTK_BOX(vbox), widgets->entry_topic, FALSE, FALSE, 0);
+  
   GtkWidget *goal_prompt = gtk_label_new("What is the goal of this session?");
   gtk_box_pack_start(GTK_BOX(vbox), goal_prompt, FALSE, FALSE, 0);
 
+  widgets->entry_goal = gtk_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(widgets->entry_goal), "Enter Goal");
+  gtk_box_pack_start(GTK_BOX(vbox), widgets->entry_goal, FALSE, FALSE, 0);
+  
   GtkWidget *start_button = gtk_button_new_with_label("Start Session");
   gtk_box_pack_start(GTK_BOX(vbox), start_button, FALSE, FALSE, 0);
-
-  widgets->entry = gtk_entry_new();
-  gtk_entry_set_placeholder_text(GTK_ENTRY(widgets->entry), "01:30:00");
-  gtk_box_pack_start(GTK_BOX(vbox), widgets->entry, FALSE, FALSE, 0);
-
 
   widgets->result_label = gtk_label_new("");
   gtk_box_pack_start(GTK_BOX(vbox), widgets->result_label, FALSE, FALSE, 0);
