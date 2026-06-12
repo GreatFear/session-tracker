18a03b124c37608e00742c0ddb1fe62ba541f300
GreatFear <no>
2026-06-12 18:33:09 -0500

    Added Topic and Goal

--- frontend.c
+++ frontend.c
@@ -3,11 +3,24 @@
 #include <stdio.h>
 
 typedef struct {
-  GtkWidget *entry;
+  GtkWidget *entry_topic;
+  GtkWidget *entry_goal;
   GtkWidget *result_label;
 } AppWidgets;
 
-//
+static void start_session(GtkWidget *button, gpointer user_data)
+{
+  AppWidgets *app = (AppWidgets *)user_data;
+
+  const char *text_topic = gtk_entry_get_text(GTK_ENTRY(app->entry_topic));
+  const char *text_goal = gtk_entry_get_text(GTK_ENTRY(app->entry_goal));
+
+  char msg[256];
+  snprintf(msg, sizeof(msg), "Topic: %s\nGoal: %s", text_topic, text_goal);
+  gtk_label_set_text(GTK_LABEL(app->result_label), msg);
+}
+
+// Starting Application
 static void start_application(GtkApplication *app, gpointer user_data)
 {
   AppWidgets *widgets = g_new0(AppWidgets, 1);
@@ -20,22 +33,34 @@ static void start_application(GtkApplication *app, gpointer user_data)
   GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
   gtk_container_add(GTK_CONTAINER(window), vbox);
 
+  // Adds a label 
   GtkWidget *topic_prompt = gtk_label_new("What is the topic of this session?");
   gtk_box_pack_start(GTK_BOX(vbox), topic_prompt, FALSE, FALSE, 0);
 
+  // Adds a entry
+  widgets->entry_topic = gtk_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(widgets->entry_topic), "Enter Topic");
+  gtk_box_pack_start(GTK_BOX(vbox), widgets->entry_topic, FALSE, FALSE, 0);
+
+  // Adds a label
   GtkWidget *goal_prompt = gtk_label_new("What is the goal of this session?");
   gtk_box_pack_start(GTK_BOX(vbox), goal_prompt, FALSE, FALSE, 0);
 
+  // Adds a entry
+  widgets->entry_goal = gtk_entry_new();
+  gtk_entry_set_placeholder_text(GTK_ENTRY(widgets->entry_topic), "Enter Goal");
+  gtk_box_pack_start(GTK_BOX(vbox), widgets->entry_goal, FALSE, FALSE, 0);
+
+  // Adds a button
   GtkWidget *start_button = gtk_button_new_with_label("Start Session");
   gtk_box_pack_start(GTK_BOX(vbox), start_button, FALSE, FALSE, 0);
 
-  widgets->entry = gtk_entry_new();
-  gtk_entry_set_placeholder_text(GTK_ENTRY(widgets->entry), "01:30:00");
-  gtk_box_pack_start(GTK_BOX(vbox), widgets->entry, FALSE, FALSE, 0);
-
-
   widgets->result_label = gtk_label_new("");
   gtk_box_pack_start(GTK_BOX(vbox), widgets->result_label, FALSE, FALSE, 0);
+  
+  g_signal_connect(start_button, "clicked", G_CALLBACK(start_session), widgets);
+
+  g_signal_connect_swapped(window, "destroy", G_CALLBACK(g_free), widgets);
 
   gtk_widget_show_all(window);
 }
